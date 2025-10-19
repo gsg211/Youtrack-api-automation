67423f3fe392b789ba18c03aaf499238ea420745
gsg211 <goreagabi8@gmail.com>
2025-10-19 11:07:50 +0300

    show only unread notifications

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ import os
 from dotenv import load_dotenv
 
 
-def decode_notification(notification):
-    decoded_data = gzip.decompress(base64.b64decode(notification))
+class Notification:
+    def __init__(self, id, content):
+        self.id = id
+        self.content = content
+
+
+def decode_notification(notification_text):
+    decoded_data = gzip.decompress(base64.b64decode(notification_text))
     soup = BeautifulSoup(decoded_data, "html.parser")
     decoded_notification = ""
     for paragraph in soup.find_all("p"):
@@ -17,31 +23,53 @@ def decode_notification(notification):
     return decoded_notification
 
 
-if __name__=="__main__":
-    load_dotenv()
+def load_sent_notifications():
+    with open('SentNotifications.text', 'r') as file:
+        id_list = file.readlines()
+    return id_list
 
-    YOUTRACK_URL = os.getenv("YOUTRACK_URL")
-    TOKEN = os.getenv("TOKEN")
 
+def write_sent_notification(notificationID):
+    with open('SentNotifications.text', 'a') as file:
+        file.writelines(notificationID + '\n')
+
+
+def get_notifications(url, token):
+    endpoint = f"{url}/api/users/me/notifications"
     headers = {
-        "Authorization": f"Bearer {TOKEN}",
+        "Authorization": f"Bearer {token}",
         "Accept": "application/json"
     }
-
     params = {
-        "filter": "read==false",
-        "fields": "content"
+        "fields": "content,id",
     }
 
-    response = requests.get(YOUTRACK_URL, headers=headers, params=params)
-    notifications_list=list()
+    return requests.get(endpoint, headers=headers, params=params)
+
+def handle_response(response):
     if response.status_code == 200:
-        response_content=response.json()
-        content=response_content[1].get("content")
+        response_content = response.json()
 
+        for notificationItem in response_content:
+            notification_id = notificationItem.get("id")
+            notification_content = decode_notification(notificationItem.get("content"))
+            notification = Notification(notification_id, notification_content)
 
-        for notification in response_content:
-            print(decode_notification(notification.get("content")))
-
+            if notification.id + "\n" not in notifications_list:
+                notifications_list.append(notification.id)
+                write_sent_notification(notification.id)
     else:
         print(f"Error: {response.status_code} - {response.text}")
+
+
+if __name__ == "__main__":
+
+    load_dotenv()
+    YOUTRACK_URL = os.getenv("YOUTRACK_URL")
+    TOKEN = os.getenv("TOKEN")
+    notifications_list = load_sent_notifications()
+
+    response = get_notifications(YOUTRACK_URL, TOKEN)
+    handle_response(response)
+
+    print(notifications_list)
